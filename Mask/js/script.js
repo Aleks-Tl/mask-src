79aede74e2babaaf338fba841ffe24eb3571fca1
Alesandr Tloka <aleksandr.tloka@i.ua>
2021-09-15 22:09:43 +0300

    finished blog screen

--- Mask/js/script.js
+++ Mask/js/script.js
@@ -41,3 +41,31 @@ window.addEventListener('DOMContentLoaded', () => {
         })
     })
 });
+
+var swiper = new Swiper(".blog__swiper", {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        1800: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    }
+});
